8bda92ac3ffb88c9aa6a12b9a2a0fa4a2c7020d1
Jayasankar C B <cbjayasankar@gmail.com>
2025-08-26 14:37:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,13 +8,15 @@ app = Flask(__name__)
 
 # Google Sheets Setup
 
-credentials_info = json.loads(os.environ['GOOGLE_SHEETS_JSON'])
-
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 SPREADSHEET_ID = '17JO6P0OkcEIH4RCCQLjcNqYsuetw5f0kAfITko629Rc'
 SHEET_NAME = 'Sheet1'
 
+with open('/run/secrets/google_sheets_json.json') as f:
+    credentials_info = json.load(f)
+
 creds = service_account.Credentials.from_service_account_info(credentials_info, scopes=SCOPES)
+
 service = build('sheets', 'v4', credentials=creds)
 sheet = service.spreadsheets()
 
